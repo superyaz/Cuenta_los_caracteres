0f2c514d45f07810f862844212f092ff837c3da7
superyaz <yacamocursos@hotmail.com>
2019-05-20 02:37:58 -0500

    Se termina la logica en el app.js

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-'use strict'
-
+function contar (texto, caracter){
+  var cont = 0;
+  for(var i=0; i<texto.length; i++){
+  if (texto[i] == caracter){
+    cont = cont + 1;
+    }
+  }
+  return (cont);
+}
+var resultado = contar("mañana", "a");
+console.log(resultado);
